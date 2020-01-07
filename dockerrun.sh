3f3aaa84305d0d92351b52eca19113e837126f46
phani <phanikumarp@users.noreply.github.com>
2020-01-07 12:30:11 +0530

    Update dockerrun.sh

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ##====> for simple restapp
-(cd /opt;nohup java  -Dserver.port=8080 -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &)
+#(cd /opt;nohup java  -Dserver.port=8080 -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &)
 #sudo chmod 775 /var/log/rest_service.log
 #sudo tail -f /var/log/rest_service.log &
 
@@ -10,7 +10,7 @@
 #sudo tail -f /var/log/rest_service.log &
 
 ###====> for newrelic
-#(cd /opt;nohup java  -Dserver.port=8080  -javaagent:/opt/newrelic/newrelic.jar -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &)
+(cd /opt;nohup java  -Dserver.port=8080  -javaagent:/opt/newrelic/newrelic.jar -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &)
 sudo chmod 775 /var/log/rest_service.log
 sudo tail -f /var/log/rest_service.log &
 
@@ -19,6 +19,4 @@ sudo tail -f /var/log/rest_service.log &
 #nohup stress-ng --vm 4 &
 #sed -i "s/# hostname: mymachine.mydomain/$hostname/g" /etc/dd-agent/datadog.conf
 #service datadog-agent start
-while true; do
-  sleep 100
-done
+sleep infinity
