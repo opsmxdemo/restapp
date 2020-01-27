012f100a3b296c0e9098bed0fd2e49d7dfe49da6
phani <phanikumarp@users.noreply.github.com>
2020-01-27 15:09:27 +0530

    Update postInstall.sh

--- pkg_scripts/rpm/postInstall.sh
+++ pkg_scripts/rpm/postInstall.sh
@@ -4,7 +4,7 @@
 ln -s /opt/restapp/restapp-0.1.0.jar /etc/init.d/restapp
 #chkconfig:  2345 24 96
 #description: restapp auto start-stop script.
-chkconfig --level 2345 restapp
+chkconfig --level 35 restapp on 
 #chkconfig --override  restapp
 sudo /etc/init.d/restapp start
 
