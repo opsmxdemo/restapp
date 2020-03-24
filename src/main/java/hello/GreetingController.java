85323d3dd64422dc5f8cdcb04bb782791a773b81
opsmxdemo <60219075+opsmxdemo@users.noreply.github.com>
2020-03-24 16:47:34 +0530

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,5 +1,5 @@
 package hello;
-// my comments more comments less comments fewer
+// my comments more comments less comments fewer newer
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
